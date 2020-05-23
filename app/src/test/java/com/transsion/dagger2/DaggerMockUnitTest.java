d82baaeed41650a988174eb65203d03a224ed003
xuecci <xuecci@gmail.com>
2020-05-23 17:49:20 +0800

    add mock demo

--- app/src/test/java/com/transsion/dagger2/DaggerMockUnitTest.java
+++ app/src/test/java/com/transsion/dagger2/DaggerMockUnitTest.java
@@ -6,8 +6,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnit;
-import org.mockito.junit.MockitoRule;
 
 import it.cosenonjaviste.daggermock.DaggerMockRule;
 import it.cosenonjaviste.daggermock.InjectFromComponent;
@@ -18,17 +16,19 @@ import it.cosenonjaviste.daggermock.InjectFromComponent;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(JUnit4.class)
-public class MockUnitTest {
+public class DaggerMockUnitTest {
 
     @Rule
-    public MockitoRule rule = MockitoJUnit.rule();
+    public final DaggerMockRule<LoginComponent> rule = new DaggerMockRule<>(LoginComponent.class, new LoginModule());
 
     @Mock
     UserManager userManager;
 
+    @InjectFromComponent
+    LoginPresenter loginPresenter;
+
     @Test
     public void testLogin() {
-        LoginPresenter loginPresenter = new LoginPresenter(userManager);
         loginPresenter.login("Ryan","12345");
         Mockito.verify(userManager).performLogin("Ryan","12345");
     }
